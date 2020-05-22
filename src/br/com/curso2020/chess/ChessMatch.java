8330d65c98f656fa56fe58bebecdfed73d60c0ff
SamuelsjO <samucagm@rocketmail.com>
2020-05-21 22:48:16 -0300

    ChessExecption and ChessPosition

--- src/br/com/curso2020/chess/ChessMatch.java
+++ src/br/com/curso2020/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package br.com.curso2020.chess;
 
 import br.com.curso2020.boardgame.Board;
-import br.com.curso2020.boardgame.Position;
 import br.com.curso2020.chess.pieces.King;
 import br.com.curso2020.chess.pieces.Rook;
 
@@ -23,9 +22,13 @@ public class ChessMatch {
 		}
 		return mat;
 	}
+	
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
 	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
-		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 8, new King(board, Color.BLACK));
+		placeNewPiece('e',1,new King(board, Color.WHITE));
 	}
 }

4c1f3649df52e820030faa356cb22cacc6e0dcfb
Samuel Oliveira <soliveira@cpqd.com.br>
2020-08-25 19:46:34 -0300

    piece move count

--- project_chessy_java/src/application/UI.java
+++ project_chessy_java/src/application/UI.java
@@ -14,7 +14,6 @@ import chess.Color;
 public class UI {
 
 	// https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
-	// iniciando projeto xadrez
 
 	public static final String ANSI_RESET = "\u001B[0m";
 	public static final String ANSI_BLACK = "\u001B[30m";
--- project_chessy_java/src/chess/ChessPiece.java
+++ project_chessy_java/src/chess/ChessPiece.java
@@ -7,6 +7,7 @@ import boardgame.Position;
 public abstract class ChessPiece extends Piece {
 
 	private Color color;
+	private int moveCount;
 
 	public ChessPiece(Board board, Color color) {
 		super(board);
@@ -16,14 +17,31 @@ public abstract class ChessPiece extends Piece {
 	public Color getColor() {
 		return color;
 	}
-	
-	protected boolean isThereOpponentPiece(Position position){
-		ChessPiece p = (ChessPiece)getBoard().piece(position);
+
+	public int getMoveCount() {
+		return moveCount;
+	}
+
+	public void setMoveCount(int moveCount) {
+		this.moveCount = moveCount;
+	}
+
+	public void decreaseMoveCount() {
+		moveCount--;
+	}
+
+	public void increaseMoveCount() {
+		moveCount++;
+	}
+
+	protected boolean isThereOpponentPiece(Position position) {
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
 		return p != null && p.getColor() != color;
 	}
-	
-	public ChessPosition getChessPosition(){
+
+	public ChessPosition getChessPosition() {
 		return ChessPosition.fromPosition(position);
-		
+
 	}
+
 }
